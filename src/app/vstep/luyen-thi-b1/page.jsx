ebd2840d7039728e03d65b548203bb5b41d16718
chien1510i68 <doducchieni86@gmail.com>
2023-08-17 13:30:25 +0700

    banner vstep b1

--- src/app/vstep/luyen-thi-b1/page.jsx
+++ src/app/vstep/luyen-thi-b1/page.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import BannerVstepB1 from '@/component/banner/BannerVstepB1'
 
 function PageVstepB1(props) {
     return (
         <div>
-            
+            <BannerVstepB1/>
         </div>
     );
 }
